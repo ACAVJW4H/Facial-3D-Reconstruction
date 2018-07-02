cd86928adbeaa74d93c7943c07223c20788bd9b0
Alexander Lattas <a.g.lattas@gmail.com>
2018-07-02 12:50:46 +0100

    use full paths to exe, previous made errors with blender

--- pipeline.bat
+++ pipeline.bat
@@ -1,4 +1,4 @@
-@echo off
+REM @echo off
 REM @author Alexander Lattas
 REM alexandros.lattas17@imperial.ac.uk
 REM Project on github.com/lattas/facial-3d-reconstruction
@@ -8,14 +8,14 @@ REM ---
 REM Create Model
 REM ---
 REM Set Paths
-SET pathToAgisoft="C:\Program Files\Agisoft\PhotoScan Pro"
-SET pathToMeshLab="..\meshlab"
-SET pathToBlender="C:\Program Files\Blender Foundation\Blender"
+SET Photoscan="C:\Program Files\Agisoft\PhotoScan Pro\photoscan.exe"
+SET Meshlab="..\meshlab\meshlabserver.exe"
+SET Blender="C:\Program Files\Blender Foundation\Blender\blender.exe"
 REM Begin
 REM ---
 REM Reconstruct 3D Model with Photoscan Agisoft
 REM The fully illuminated, cross-pollarized photos should be in createModel.py's directory, names card{}.JPG
-REM %pathToAgisoft%\photoscan.exe -r createModel.py
+REM %Photoscan% -r createModel.py
 REM ECHO 3D MODEL RECONSTRUCTED
 REM Compute diffuse and specular Normal Maps
 REM Photos should be in ..\data\card{}\1-16.TIF
@@ -25,22 +25,23 @@ REM Compose Meshlab diffuse project,
 REM execute the blenderScript to create the texture map from the diffuse normals and
 REM connect their coordinates with the model.
 python photometricNormals.py --diffuseProj
-%pathToMeshLab%\meshlabserver.exe -p specularProject.mlp -i agisoftExport.obj -s blenderScript.mlx -w forBlender.mlp
+%Meshlab% -p specularProject.mlp -i agisoftExport.obj -s blenderScript.mlx -w forBlender.mlp
 ECHO MESHLAB DIFFUSE PROJECT EXECUTED
 REM Compose Meshlab specular project,
 REM execute the blenderScript to create the texture map from the specular normals and
 REM connect their coordinates with the model.
 python photometricNormals.py --specularProj
-%pathToMeshLab%\meshlabserver.exe -p specularProject.mlp -i agisoftExport_out.obj -s blenderScript.mlx -w specularAdded.mlp
+%Meshlab% -p specularProject.mlp -i agisoftExport_out.obj -s blenderScript.mlx -w specularAdded.mlp
 ECHO MESHLAB SPECULAR PROJECT EXECUTED
 REM Apply Gaussian blur of radius 1
 python blur.py
 ECHO BLUR APPLIED.
 ECHO CONVERT SPECULAR MAP TO DISPLACEMENT MAP WITH SHADER MAP 4 AND PRESS A KEY.
+PAUSE
 REM Before continuing, the specular normal texture map should be imported to Shader Map 4
 REM and converted by hand to a displacement map with height 100 and contrast 108-112.
 REM The result should overwrite the input file with the same name in same location.
 REM ---
 REM Execute the blender script to create detail on the model using the texture map.
-%pathToBlender%\blender.exe -b -P blender.py
+%blender% -b -P blender.py
 ECHO BLENDER SCRIPT EXECUTED.
