b46ddcc486e008ac6aa6aa5be5781ff1924977a0
Alexander Lattas <a.g.lattas@gmail.com>
2018-07-03 12:34:02 +0100

    organise photos for new file directory

--- organisePhotos.py
+++ organisePhotos.py
@@ -12,7 +12,7 @@ if CAM_NUM not in [9, 10]:
     print("Only 9 and 10 cameras supported.")
     exit()
 
-PHOTOS = "../testdata"
+PHOTOS = "../data"
 CARDS = ["card{}".format(i) for i in range(1, CAM_NUM+1)]
 NAMES  = ["{}.CR2".format(i) for i in range(1, 17)]
 
@@ -21,19 +21,23 @@ for card in CARDS:
     
     for i in range(1, 17):
         # rename images to card{} folders
-        # os.rename(os.path.join(cardpath, photos[i]),
-        #        os.path.join(cardpath, NAMES[i]))
+        new_image = os.path.join(cardpath, NAMES[i])
+        os.rename(os.path.join(cardpath, photos[i]), new_image)
     
+        with rawpy.imread(new_image) as raw:
+            rgb = raw.postprocess(gamma=(1,1), no_auto_bright=True)
+        imageio.imsave(cardpath{}.format(i)+'.TIF', rgb)
+
         # 11th for the 10 camera setting
         # 15th for the 9  camera setting
         if CAM_NUM==9:
             if (i == 15):
                 copyfile(os.path.join(cardpath, "15.tif"),
-                        "{}.tif".format(card))
+                        "{}.TIF".format(card))
         else:
             if (i == 11):
                 copyfile(os.path.join(cardpath, "15.tif"),
-                        "{}.tif".format(card))
+                        "{}.TIF".format(card))
 
     
     print("Card {} done.".format(card))
