f85e827da7b6afba1769ba12f4bdc21f8fb45f7c
Alexander Lattas <a.g.lattas@gmail.com>
2018-07-09 16:41:38 +0100

    save colors to pickle

--- separateColor.py
+++ separateColor.py
@@ -1,3 +1,5 @@
+import pickle
+
 with open("../data/agisoftExport.obj", "r") as agisoft:
     vertices = agisoft.readlines()
     vertices = vertices[2:]
@@ -7,11 +9,7 @@ for line in vertices:
     items = line.split(" ")
     if items[0] != "v":
         break
-    col.append(items[4:7])
+    col.append([float(items[4]), float(items[5]), float(items[6])])
 
-with open("../data/agisoftExportColors.csv", 'w') as output:
-    out = []
-    for co in col:
-        out.append(str(co[0])+","+str(co[1])+","+str(co[2]))
-    output.writelines(out)
-print("done")
+pkl_file=open("../data/agisoftExportColors.pkl", "wb")
+pickle.dump(col, pkl_file)
