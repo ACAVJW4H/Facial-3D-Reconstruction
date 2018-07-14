928ad08e0a83d71a00c389e49ce5ea35e0f066b2
Alexander Lattas <a.g.lattas@gmail.com>
2018-07-14 17:54:28 +0100

    Structure pipeline messages

--- pipeline.bat
+++ pipeline.bat
@@ -4,46 +4,57 @@ REM alexandros.lattas17@imperial.ac.uk
 REM Project on github.com/lattas/facial-3d-reconstruction
 REM Reconstructs a 3D model of a face using pictures of different illumination and angle, as taken by a light stage.
 REM Based on Mridul Kumar's MEng thesis project
-REM ---
-REM Create Model
-REM ---
-REM Set Paths
+REM ------------------Paths---------------------
+REM Set Paths to external Applications
 SET Photoscan="C:\Program Files\Agisoft\PhotoScan Pro\photoscan.exe"
 SET Meshlab="..\meshlab\meshlabserver.exe"
 SET Blender="C:\Program Files\Blender Foundation\Blender\blender.exe"
 SET ShaderMap4="C:\Program Files\ShaderMap 4\bin\ShaderMap.exe"
 REM Begin
-REM ---
+REM --------------Reconstruction----------------
 REM Reconstruct 3D Model with Photoscan Agisoft
 REM The fully illuminated, cross-pollarized photos should be in createModel.py's directory, names card{}.JPG
+ECHO STARTING: 3D model reconstruction with Agisoft Photoscan
 %Photoscan% -r createModel.py
-ECHO 3D MODEL RECONSTRUCTED
+ECHO DONE:     3D model reconstruction as agisoftExport.obj
+REM ------------------Normals-------------------
 REM Compute diffuse and specular Normal Maps
 REM Photos should be in ..\data\card{}\1-16.TIF
+ECHO STARTING: Computing diffuse and specular normal maps
 python photometricNormals.py --maps
-ECHO DIFFUSE AND SPECULAR MAPS COMPUTED
+ECHO DONE:     Diffuse and specular normal maps
+REM ---------------diffuseProject---------------
 REM Compose Meshlab diffuse project,
 REM execute the blenderScript to create the texture map from the diffuse normals and
 REM connect their coordinates with the model.
+ECHO STARTING: Executing diffuseProject with meshlab
 python photometricNormals.py --diffuseProj
 %Meshlab% -p ..\data\diffuseProject.mlp -i ..\data\agisoftExport.obj -s blenderScript.mlx -w ..\data\diffuseAdded.mlp
-ECHO MESHLAB DIFFUSE PROJECT EXECUTED
+ECHO DONE:     Meshlab diffuseProject 
+REM --------------specularProject---------------
 REM Compose Meshlab specular project,
 REM execute the blenderScript to create the texture map from the specular normals and
 REM connect their coordinates with the model.
+ECHO STARTING: Executing specularProject with meshlab
 python photometricNormals.py --specularProj
 %Meshlab% -p ..\data\specularProject.mlp -i ..\data\agisoftExport_out.obj -s blenderScript.mlx -w ..\data\specularAdded.mlp
 MOVE blenderTexture.png ..\data\blenderTexture.png
-COPY ..\data\blenderTexture.png ..\data\OriginalBlenderTexture.png
-ECHO MESHLAB SPECULAR PROJECT EXECUTED
+COPY ..\data\blenderTexture.png ..\data\blenderTextureBKP.png
+ECHO DONE:     Meshlab specularProject and blenderTexture
+REM --------------------Blur---------------------
 REM Apply Gaussian blur of radius 1
+ECHO STARTING: Applying blur on blenderTexture
 python blur.py
-ECHO BLUR APPLIED.
-REM Before continuing, the specular normal texture map should be imported to Shader Map 4
-REM and converted by hand to a displacement map with height 100 and contrast 108-112.
-REM The result should overwrite the input file with the same name in same location.
-%ShaderMap% shadermap.lua
-ECHO DISPLACEMENT MAP CREATED.
-REM Execute the blender script to create detail on the model using the texture map.
+ECHO DONE:     blur applyed on blenderTexture
+REM --------------DisplacementMap-----------------
+REM The blenderTexture specular normal map is converted
+REM to a displacement map with ShaderMap 4, overwriting the input.
+ECHO STARTING: Computing displacement map with ShaderMap
+%ShaderMap4% shadermap.lua
+ECHO DONE:     Displacement map computed as blenderTexture
+REM -----------------Subdivision------------------
+REM The displacement map is used, along with its coordinates on the model,
+REM as embedded by meshlab, to do a subdivision on it, to create details such as pores and wrinkles.
+ECHO STARTED: Subdividing AgisoftExport with Blender
 %blender% -b -P blender.py
-ECHO BLENDER SCRIPT EXECUTED.
+ECHO DONE:    Final model produced as final.obj
