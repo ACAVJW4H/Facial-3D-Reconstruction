d94feddf3bbf2734f3f073b00c0620d7ce2359f3
Alexander Lattas <a.g.lattas@gmail.com>
2018-08-02 18:36:04 +0100

    anonymize data

---
+++ anonymise.py
@@ -0,0 +1,87 @@
+import os
+from argparse import ArgumentParser
+
+EXPRESSIONS = [
+    '_plain',
+    '_happy',
+    '_sad',
+    '_angry',
+    '_disgusted'
+]
+
+FILES = [
+    '_LQ.obj',
+    '_HQ.obj',
+    '.zip'
+]
+
+def get_size(filename):
+    file_stats = os.stat(filename)
+    return file_stats.st_size
+
+def update_counter():
+    name_counter += 1
+
+def get_files_to_rename():
+
+    basedir = os.getcwd()
+
+    all_files = os.listdir(basedir)
+    files_to_rename = []
+
+    # files with name contain dashes in the date
+    for file_name in all_files:
+        if "-" in file_name and ".zip" in file_name:
+            files_to_rename.append(
+                fine_name.split("_")[0]
+                + "_" +
+                fine_name.split("_")[1]
+            )
+    
+    return files_to_rename
+
+def rename_file(src, dest):
+    '''
+    Rename a single file and check it was successful.
+    '''
+    size_before = get_size(src)
+
+    assert(not os.path.exists(dest))
+    os.rename(src, dest)
+
+    size_after = get_size(dest)
+    assert(size_before == size_after)
+
+    print(dest + " renamed.")
+
+def rename_all(files_to_rename):
+
+    cwd = os.getcwd()
+
+    for base_name in files_to_rename:
+        for expression in EXPRESSIONS:
+
+            src_base = os.path.join(cwd, base_name)
+            src_base_exp = src_base + expression
+            
+            dest_base = os.path.join(cwd, str(name_counter))
+            dest_base_exp = dest_base + expression
+
+            for file_name in FILES:
+                src = src_base_exp + file_name
+                dest = dest_base_exp + file_name
+                print(src)
+                print(dest)
+                #rename_file(src, dest)
+
+        update_counter()
+
+
+parser = ArgumentParser()
+parser.add_argument("-n", "--starting_number", required=True, help="The number name to start")
+args = parser.parse_args()
+
+global name_counter = args.starting_number
+
+files_to_rename = get_files_to_rename()
+rename_all(files_to_rename)
