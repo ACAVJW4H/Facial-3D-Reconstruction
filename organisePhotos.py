32680121e0229ec44d36df2b727ad63310f65e52
Alexander Lattas <a.g.lattas@gmail.com>
2018-10-30 14:31:38 +0000

    organise for the 10-pic setting

--- organisePhotos.py
+++ organisePhotos.py
@@ -10,37 +10,29 @@ from shutil import copyfile
 import sys
 import warnings
 from progress.bar import Bar
+import pickle
+from argparse import ArgumentParser
 
 # Set number of cameras
 CAM_NUM = 9
+VALUES_DIR = "./white_balancing_values.pkl"
 
 # Set other constants
 PHOTOS = "../data"
 CARDS = ["card{}".format(i) for i in range(1, CAM_NUM+1)]
-NAMES  = ["{}.tif".format(i) for i in range(1, 17)]
+NAMES  = ["{}.tif".format(i) for i in range(1, 11)]
 
-# Set variables of color correction, to catch the white block
-STARTHEIGHT = 2676 # Upper left pixel
-STARTWIDTH  = 2205 # Upper left pixel
-DISTANCE    = 130  # Travel (pixels) to lower right pixel
+RED_CORRECTION = 1.
+GREEN_CORRECTION = 1.
+BLUE_CORRECTION = 1.
 
 if CAM_NUM==9:
-    STEREO_INPUT = 7 #15
+    STEREO_INPUT = 9 #15
 elif CAM_NUM==10:
     STEREO_INPUT = 11
 else:
     print("Only 9 and 10 cameras supported.")
     exit()
-
-# RED_CORRECTION = 0.9 / 0.57508159
-# GREEN_CORRECTION = 0.9 / 0.33863589
-# BLUE_CORRECTION = 0.9 / 0.30628449
-# PHOTOS = "../comp_c_grad"
-
-RED_CORRECTION = 0.9 / 0.68484205
-GREEN_CORRECTION = 0.9 / 0.38325126
-BLUE_CORRECTION = 0.9 / 0.11427009
-PHOTOS = "../comp_c_bin/"
 
 # imageio throws multiple warnings that do not affect functionality
 if not sys.warnoptions:
@@ -53,25 +45,43 @@ def save_np_image(im, name):
     im = Image.fromarray(im.astype('uint8'))
     im.save(name)
 
-def get_white_balance_correction_values(startHeight = STARTHEIGHT, startWidth = STARTWIDTH, distance = DISTANCE):
+def get_white_balance_correction_values(chartdir, startWidth, startHeight, distance):
     '''
     Use the white box in the cross-pollarized fully iluminated capture
     to find an average correction measure for automatic white balance.
     @author Mingqian
     '''
-    colorChart = np.array(Image.open("../dataColors/card4/15.tif")).astype("float64")
-    sum = np.array([0.,0.,0.])
+    
+    wb_photo = get_raw_photos("../{}/card4".format(chartdir))[-2]
+    wb_photo_tif = wb_photo.split(".CR2")[0] + ".tif"
+
+    with rawpy.imread("../{}/card4/{}".format(chartdir, wb_photo)) as raw:
+        rgb = raw.postprocess(use_auto_wb=False, gamma=(1,1), no_auto_bright=True, output_bps=8)
+    imageio.imsave("../{}/card4/{}".format(chartdir, wb_photo_tif), rgb)
+
+    colorChart = np.array(Image.open("../{}/card4/{}".format(chartdir, wb_photo_tif))).astype("float64")
+
+    # test = colorChart
+    # test[startHeight : startHeight+distance, startWidth : startWidth + distance, ...] = [245., 0., 0.]
     test = colorChart[startHeight : startHeight+distance, startWidth : startWidth + distance, ...]
-    save_np_image(test, "../test.tif")
+
+    save_np_image(test, "test.tif")
+ 
+    sum = np.array([0.,0.,0.])
     #average value of a 200x200 patch
     for h in range(startHeight, startHeight + distance):
         for w in range(startWidth, startWidth + distance):
             sum[0] += colorChart[h,w,0]
             sum[1] += colorChart[h,w,1]
             sum[2] += colorChart[h,w,2]
-    sum = sum / (float(distance) * float(distance) * 255.)
 
+    sum = sum / (float(distance) * float(distance) * 255.)
+    sum = 0.9 / sum
+    save_np_image(get_white_balance_image(test, sum[0], sum[1], sum[2]), "testwb.tif")
     print(sum)
+
+    with open(VALUES_DIR, 'wb') as values_file:
+        pickle.dump(sum, values_file)
 
 # 0.90^3 is the greish white as appears on the camera with correct white balance
 # The other three values come from get_white_balance_correction_values().
@@ -227,11 +237,16 @@ def organise_raw_photos():
         bar.next()
     bar.finish()
 
-# get_white_balance_correction_values()
-organise_raw_photos()
+parser = ArgumentParser()
+parser.add_argument("-o", "--operation", default="organise", help="if given, get white balance values for that capture")
+parser.add_argument("-x", "--x", help="Upper left X of white square")
+parser.add_argument("-y", "--y", help="Upper left Y of white square")
+parser.add_argument("-d", "--distance", default=100, help="Distance accross the white square")
+args = parser.parse_args()
 
-# RED_CORRECTION = 0.9 / 0.68484205
-# GREEN_CORRECTION = 0.9 / 0.38325126
-# BLUE_CORRECTION = 0.9 / 0.11427009
-# PHOTOS = "../bin_c_grad/"
-# organise_raw_photos()
+if args.operation == "organise":
+    with open(VALUES_DIR, 'rb') as values_file:
+        RED_CORRECTION, GREEN_CORRECTION, BLUE_CORRECTION = pickle.load(values_file)
+    organise_raw_photos()
+else:
+    get_white_balance_correction_values(args.operation, int(args.x), int(args.y), int(args.distance))
